09eaa8a57e4d1131d9886419bf59a61d1f279a76
kaluzsikc <kaluzsikc@gmail.com>
2018-10-18 13:26:06 +0100

    Added Barbarian, Dwarf,Fighter and Knight class

--- src/main/java/Players/Fighters/Dwarf.java
+++ src/main/java/Players/Fighters/Dwarf.java
@@ -1,4 +1,25 @@
 package Players.Fighters;
 
-public class Dwarf {
+import Interfaces.IAttack;
+import Interfaces.IDefend;
+import Items.Item;
+import Players.Player;
+
+import java.util.ArrayList;
+
+public class Dwarf extends Player implements IAttack, IDefend {
+    public Dwarf(String name, int hitPoints, ArrayList<Item> bag){
+        super(name, hitPoints, bag);
+    }
+
+    @Override
+    public int attack(IDefend target) {
+        return 0;
+    }
+
+
+    @Override
+    public void defend(IAttack attacker) {
+
+    }
 }
--- src/main/java/Players/Fighters/Knight.java
+++ src/main/java/Players/Fighters/Knight.java
@@ -1,4 +1,25 @@
 package Players.Fighters;
 
-public class Knight {
+import Interfaces.IAttack;
+import Interfaces.IDefend;
+import Items.Item;
+import Players.Player;
+
+import java.util.ArrayList;
+
+public class Knight extends Player implements IAttack, IDefend {
+    public Knight(String name, int hitPoints, ArrayList<Item> bag) {
+        super(name, hitPoints, bag);
+    }
+
+    @Override
+    public int attack(IDefend target) {
+        return 0;
+    }
+
+
+    @Override
+    public void defend(IAttack attacker) {
+
+    }
 }
