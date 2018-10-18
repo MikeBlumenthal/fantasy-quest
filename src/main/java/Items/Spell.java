4d71e51b51efb1015d455c9473e040b4270422bc
Ben <benharvie94@gmail.com>
2018-10-18 13:56:30 +0100

    Spell tests passing

--- src/main/java/Items/Spell.java
+++ src/main/java/Items/Spell.java
@@ -1,7 +1,15 @@
 package Items;
 
 public class Spell extends Item  {
-    public Spell(String name) {
+
+    private int damage;
+
+    public Spell(String name, int damage) {
         super(name);
+        this.damage = damage;
+    }
+
+    public int getDamage() {
+        return damage;
     }
 }
