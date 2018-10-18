bf849690978448aaa541e50b822b6014a966edf1
Ben <benharvie94@gmail.com>
2018-10-18 14:42:55 +0100

    Barbarian class completed and tested

--- src/test/java/BarbarianTest.java
+++ src/test/java/BarbarianTest.java
@@ -1,2 +1,39 @@
+import Items.Weapon;
+import Players.Fighters.Barbarian;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BarbarianTest {
+
+    Barbarian barbarian;
+    Weapon weapon;
+
+    @Before
+    public void before() {
+        barbarian = new Barbarian("Bobby the Barbaric", 900);
+        weapon = new Weapon("Longsword", 20);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Bobby the Barbaric", barbarian.getName());
+    }
+
+    @Test
+    public void hasHitPoints() {
+        assertEquals(900, barbarian.getHitPoints());
+    }
+
+    @Test
+    public void startWithNoWeapon() {
+        assertEquals(null, barbarian.getInHand());
+    }
+
+    @Test
+    public void weildsWeapon() {
+        barbarian.changeWeapon(weapon);
+        assertEquals("Longsword", barbarian.getInHand().getName());
+    }
 }
