683a7de30c5412542be115ab7fe642d87f26bb30
Michael Blumenthal <bloomer25@googlemail.com>
2018-10-18 13:04:04 +0100

    interfaces done

--- src/main/java/Interfaces/IAttack.java
+++ src/main/java/Interfaces/IAttack.java
@@ -1,4 +1,5 @@
 package Interfaces;
 
 public interface IAttack {
+    int attack(IDefend target);
 }
--- src/main/java/Interfaces/IDefend.java
+++ src/main/java/Interfaces/IDefend.java
@@ -1,4 +1,5 @@
 package Interfaces;
 
 public interface IDefend {
+    void defend(IAttack attacker);
 }
